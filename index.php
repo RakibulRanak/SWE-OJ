ddcfa58ceb56ff4e859d488914ff6aed025dd084
RakibulRanak <rakibulhasanranak1@gmail.com>
2019-09-10 23:14:36 +0600

    started

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  
+    
+       <!--  <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge"> -->
+        <title>Ranak</title>
+        <!-- <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/6d42579467.js"></script>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/ranak.css">
+     <!--    <link rel="icon" type="image/png" href="img/xxx.png"> -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="  sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="      sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+
+ 
+<body>
+  <div class ="main">
+    <div class="row">
+        <div class="col-md-12">
+           <nav class="shadow navbar navbar-dark bg-dark navbar-expand-md mytop nbar">
+                   <div class="navbar-header ">
+                       <a class="navbar-brand lspace" href="home.php">SWE OJ</a>
+
+                        <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target"><span class="navbar-toggler-icon"></span>
+                        </button>
+                                 
+                  </div>
+                   <div class="collapse navbar-collapse " id="collapse_target">
+                        <ul class="nav navbar-nav">
+                                <li class="space ranak"><a href="compiler.php"><i class="fa fa-code ispace"></i>Compiler</a></li>
+                                <li class="space"><a href="archive.php"><i class="fa fa-archive ispace"></i>Problem Archive</a></li>
+                                <li class="space"><a href="contest.php"><i class="fa fa-cogs ispace"></i>Contests</a></li>
+                                <li class="space"><a href="#"><i class="fa fa-check-square ispace"></i>Debug</a></li>
+                                <li class="lgspace space"><a href="profile.php?user=<?php echo("$username"); ?>"><i class="fa fa-user ispace"></i>RakibulRanak</a></li>
+                                <li class="space"><a href="logout.php"><i class="fa fa-power-off ispace"></i>Logout</a></li>
+      
+                         </ul>
+                   </div>
+                
+            </nav>
+       </div>
+   </div>
+ </div>
+
+</body>
+</html>
+
+
